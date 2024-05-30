72996d9b849525e51258817ecd335e828263c25a
ganeef <ganee@DESKTOP-POCL62K>
2024-05-30 11:57:33 +0200

    added Ingredient Class

---
+++ PROG-PART2-FINAL/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_PART2_FINAL
+{
+    internal class Ingredient
+    {
+    }
+}
