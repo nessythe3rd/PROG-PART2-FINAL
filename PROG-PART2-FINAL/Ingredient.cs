78ec9195ec396d9db89fbc0b2bfaadcd37000788
ganeef <ganee@DESKTOP-POCL62K>
2024-05-30 12:00:58 +0200

    geters and setters for ingredient class

--- PROG-PART2-FINAL/Ingredient.cs
+++ PROG-PART2-FINAL/Ingredient.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace PROG_PART2_FINAL
 {
-    internal class Ingredient
-    {
+        internal class Ingredient
+        {
+            public string Name { get; set; }
+            public double Calories { get; set; }
+            public string FoodGroup { get; set; }
+            public string Measurement { get; set; }
+        }
     }
-}
+
+  
+
+
